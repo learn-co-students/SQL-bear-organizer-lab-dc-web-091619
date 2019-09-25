7249dcb34f11465d398d3bdfe489774d8be626ee
Charles DeLeo <flatironschool@Flatirons-MacBook-Air.local>
2019-09-25 13:48:48 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Mr. Chocolate', 5, 'male', 'brown', 'content', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Rowdy', 7, 'male', 'black', 'playful', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Tabitha', 3, 'female', 'white', 'lovesick', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Sergeant', 4, 'male', 'chocolate', 'tense', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Melissa', 8, 'female', 'black', 'happy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Grinch', 2, 'male', 'brown', 'angry', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Wendy', 3, 'female', 'white', 'goofy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+(NULL, 9, 'male', 'brown', 'hungry', 1);
